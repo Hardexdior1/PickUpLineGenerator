e1b1681a0b980b1a4c6c6589f2af331f79bbfa0f
adeniran <adeniranquwam001@gmail.com>
2023-06-07 02:51:01 +0100

    holycommunion

--- index.js
+++ index.js
@@ -5,9 +5,29 @@ name.textContent="coded by <>'oluwadamilare </>'"
 let jokesDiv = document.querySelector(".jokesDiv");
 let generate = document.querySelector(".generate");
 let jokes = [
-  "what is the name of baby duck ......lil durk",
-  "the name of bayern pick up lines",
-  "does your mother sells okro ...because you are drawing my attention ",
+  "What's your favorite drink? I'm asking so I know what to buy you on our first date.",
+  "Do you have a name or can I call you mine?",
+  "You owe me a drink. Because when I saw you, I dropped mine.",
+  "Is your name Wi-Fi? Because I'm really feeling a connection.",
+  "Are you a magician? Whenever I look at you, everyone else disappears.",
+  "If you were a vegetable, you'd be a 'cute-cumber.'",
+  "Do you have a map? I keep getting lost in your eyes.",
+  "If you were a song, you'd be the best single on the album.",
+  "Aren't you tired? From running through my mind all day?",
+  "I don't know your name, but I'm sure it's just as beautiful as you are.",
+  "If I could rearrange the alphabet, I'd put U and I together.",
+  "I was feeling a little tired today, but you've turned me on again.",
+  "Are you a charger? Because I'm dying without you..  oraimo charger abi ..lol",
+  "I'd say God bless you, but it looks like he already has.",
+  "If you let me borrow a kiss, I promise I'll give it right back.",
+  "I'd never play hide and seek with you, because someone like you is impossible to find.",
+  "Can I have your picture so I can show Santa what I want for Christmas this year?",
+  "Not to be dramatic, but I think you're the answer to my prayers.",
+  "I think there's something wrong with my phone. Your number's not in it.",
+  "Do you have a Band-Aid? Because I just scraped my knee falling for you, and now I need some serious medical attention.",
+  "Excuse me, do you have a name or can I call you mine? Because I have a bet with my friends about how fast I can make a girl cringe.",
+  "Do you believe in fate? Because I think we were destined to meet, and by 'destined,' I mean my imaginary friend told me so.",
+
 ];
 
 
@@ -20,39 +40,12 @@ console.log(jokesAndRandom);
 jokesDiv.textContent=jokesAndRandom
 })
 
-// "Is your name Google? Because you've got everything I'm searching for."
-// "Do you have a name or can I call you mine?"
-// "Are you a magician? Whenever I look at you, everyone else disappears."
-// "Is your dad a boxer? Because you're a knockout!"
-// "Is your name Wi-Fi? Because I'm really feeling a connection."
-// "If you were a vegetable, you'd be a 'cute-cumber.'"
-// "Excuse me, but I think you dropped something: my jaw."
-// "Is your dad a baker? Because you're a cutie pie!"
-// "Do you have a map? I keep getting lost in your eyes."
-// "I must be a snowflake because I've fallen for you."
 
 
-// Apologies for the repetition earlier. Here are 20 new and crazy pickup lines for you:
-
-// 1. "Is your name Google? Because you have everything I'm searching for, including my lost sanity."
-// 2. "Are you a parking ticket? Because you've got 'fine' written all over you, and I'm guilty as charged."
-// 3. "Are you a magician? Because whenever I look at you, everyone else disappears, just like my chances of staying sane around you."
-// 4. "Do you believe in fate? Because I think we were destined to meet, and by 'destined,' I mean my imaginary friend told me so."
-// 5. "Are you a time traveler? Because every time I see you, my watch goes crazy and time seems to stand still."
-// 6. "Excuse me, do you have a name or can I call you mine? Because I have a bet with my friends about how fast I can make a girl cringe."
-// 7. "Do you have a map? Because I keep getting lost in your eyes, and Google Maps refuses to help me navigate this level of attractiveness."
-// 8. "If you were a vegetable, you'd be a cute-cumber, and I'd be a nutjob for using vegetable analogies in pickup lines."
-// 9. "Are you a dictionary? Because you add meaning to my life, or at least you would if I understood half the words you use."
-// 10. "Is your dad an artist? Because you're a masterpiece, and I'm willing to go to the crazy lengths of pretending I know anything about art."
-// 11. "Are you a magician? Because whenever I see you, everyone else disappears, and I start questioning the fabric of reality."
-// 12. "Do you have a Band-Aid? Because I just fell from heaven, and now I have a twisted ankle and a strong desire to meet you."
-// 13. "Is your dad a baker? Because you're a cutie pie, and I'm willing to risk a sugar overdose for your attention."
-// 14. "Excuse me, but I think you dropped something: my sanity. Mind helping me find it by going out with me?"
-// 15. "Is your name Wi-Fi? Because I'm feeling a strong connection, and I'm willing to trade my sanity for a stable internet connection."
-// 16. "If you were a vegetable, you'd be a 'fineapple,' and I'd be a 'crazy' fruit trying to impress you."
-// 17. "Do you have a Band-Aid? Because I just scraped my knee falling for you, and now I need some serious medical attention."
-// 18. "Is your dad a boxer? Because you're a knockout, and I'm willing to let you give me a standing eight-count of crazy love."
-// 19. "Do you have a name, or can I call you 'mine'? Because I'm about to go completely off the rails with these pickup lines."
-// 20. "Is your dad a magician? Because whenever I look at you, everyone else disappears, and I start to question the very fabric of reality... in a completely sane and not at all crazy way."
-
-// Remember to use these lines with a sense of humor and be prepared for different reactions. Not everyone may appreciate the craziness, so be attentive to the other person's response and adjust accordingly.
+// 
+// 
+// 
+// 
+//
+// 
+// 
